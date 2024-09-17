28d421c6b85aca8b510e5e4232e61a528f78a6b5
nehasingh9910 <ns812625@gmail.com>
2024-09-17 16:11:50 +0530

    Add files via upload

---
+++ python bootcamp day 2.py
@@ -0,0 +1,54 @@
+#wap for current convertor
+#before starting any program you have to think the 
+#input parameter required in programs
+#e.g in currerncy converter u need currency amount
+#in inr and converted currency index rate
+
+#print("convert rupees into dollars")
+#rupeesamount=int(input("Enter the amount the rs."))
+#rsintodollar=(rupeesamount/84)
+#print(rupeesamount,"convert into dollar",rsintodollar)
+
+
+#print("convert dollars into rupees")
+#dollaramount=int(input("Enter the amount the dollars."))
+#dollarintors=(dollaramount*84)
+#print(dollaramount,"convert into rs",dollarintors)
+
+#wap to check the eligible for voting
+#userage=int(input("enter your age"))
+#for
+
+#EmpAge=int(input("enter your age:"))
+#EmpGender=input("enter your gender:")
+#method1
+#if(EmpAge >17 and EmpGender=="female"):
+ #   print("your are eligible for govt job applicant")
+#elif(EmpAge>17 and EmpGender=="Male"):
+#      print("your are  eligible for private job applicant")
+#else:
+ #    print("you re not eligible for any job.")
+
+
+
+# a=int(input("enter a value:"))
+# b=int(input("enter b value:"))
+# c=int(input("enter c value:"))
+
+# if(a>b and a>c):
+#      print("a is a greatest number")
+
+# elif(b>a and b>c):
+#     print("b is a greatest number")     
+     
+# else:
+#    print("c is a greatest number")   
+
+#SWITCH CONDITION IS REPLACEMENT OF MULTIPLE IF ELSE CONDITION
+ 
+
+friendname=input["neha","garima","hardik",1,2,3,]
+print(type(friendname))
+friendname.append("mayank")
+print(friendname)
+                                                                                                    
